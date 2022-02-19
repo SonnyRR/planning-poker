b705086503884165aa54db7b4173b176c9ad6461
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-02-19 19:04:17 +0200

    Add initial setup for Serilog.

---
+++ PlanningPoker.SharedKernel/Enrichers/InstanceEnricher.cs
@@ -0,0 +1,20 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace PlanningPoker.SharedKernel.Enrichers
+{
+    public sealed class InstanceEnricher : ILogEventEnricher
+    {
+        private readonly Guid id;
+
+        public InstanceEnricher()
+        {
+            this.id = Guid.NewGuid();
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("InstanceId", this.id));
+        }
+    }
+}
--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using Serilog;
 
 namespace PlanningPoker.Server
 {
@@ -18,15 +19,12 @@ namespace PlanningPoker.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,14 +35,14 @@ namespace PlanningPoker.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSerilogRequestLogging();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
-
+            app.UseHttpsRedirection();
+            app.UseSerilogIngestion();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>

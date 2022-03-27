d48097ce0845d585b788f214db7d029428615285
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-03-27 12:43:29 +0300

    Add dedicated room selector page.

---
+++ src/Client/Pages/RoomSelector.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PlanningPoker.Client.Pages
+{
+    public partial class RoomSelector
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+    }
+}

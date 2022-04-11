c0eb89388dc8a6e68ef3d6e89f7dcb3b4f50559b
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-04-11 18:21:16 +0300

    Added form for entering existing room codes.

--- src/Client/Pages/RoomSelector.razor.cs
+++ src/Client/Pages/RoomSelector.razor.cs
@@ -1,10 +1,33 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Text.Json;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+using Radzen;
 
 namespace PlanningPoker.Client.Pages
 {
     public partial class RoomSelector
     {
         [Inject]
+        public ILogger<RoomSelector> Logger { get; set; }
+
+        [Inject]
         public NavigationManager NavigationManager { get; set; }
+
+        public bool ShowRoomIdentifierInput { get; set; }
+
+        public string RoomId { get; set; };
+
+        public void JoinExistingTable()
+        {
+            Logger.LogInformation("I am here");
+            //NavigationManager.NavigateTo($"/room/{tableId}");
+        }
+
+        void OnInvalidSubmit(FormInvalidSubmitEventArgs args)
+        {
+            Logger.LogError("InvalidSubmit", JsonSerializer.Serialize(args, new JsonSerializerOptions() { WriteIndented = true }));
+        }
     }
 }

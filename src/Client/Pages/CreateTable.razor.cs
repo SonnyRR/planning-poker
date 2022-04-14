69543f381cd174d8be70fa35c4834dca2079be4a
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-04-14 18:26:38 +0300

    Added a form for the creation of poker tables.

--- src/Client/Pages/CreateTable.razor.cs
+++ src/Client/Pages/CreateTable.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace PlanningPoker.Client.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+using PlanningPoker.Client.Models;
+
+namespace PlanningPoker.Client.Pages
 {
     public partial class CreateTable
     {
+        [Inject]
+        public ILogger<TableSelector> Logger { get; set; }
+
+        public TableMetadata Table { get; set; }
     }
 }
---
+++ src/SharedKernel/Models/Tables/TableMetadata.cs
@@ -0,0 +1,24 @@
+﻿namespace PlanningPoker.Client.Models
+{
+    /// <summary>
+    /// Represents the information about a given poker table.
+    /// </summary>
+    public sealed class TableMetadata
+    {
+        public TableMetadata()
+        {
+        }
+
+        public TableMetadata(string name, string code)
+        {
+            this.Name = name;
+            this.Code = code;
+        }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        // TODO: Add remaining properties.
+    }
+}

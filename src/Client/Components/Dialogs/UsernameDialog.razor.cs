eadffa678e72a659be37180fa9e26f080ac6cf4f
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-04-15 15:22:09 +0300

    Introduced a new `Username` dialog component.
    Used for entering a nickname that will be represented at a given poker table.

---
+++ src/Client/Components/Dialogs/UsernameDialog.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using Radzen;
+
+namespace PlanningPoker.Client.Components.Dialogs
+{
+    public partial class UsernameDialog
+    {
+        [Inject]
+        public DialogService DialogService { get; set; }
+
+        public string Username { get; set; }
+
+        public void OnSubmit()
+        {
+            this.DialogService.Close(this.Username);
+        }
+    }
+}

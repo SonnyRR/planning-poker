f6b57294b996afb76d82fb1f52b3b2d9872ad3f5
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-04-15 15:20:41 +0300

    Introduced `DeckType` enumeration.

---
+++ src/SharedKernel/Models/Decks/DeckType.cs
@@ -0,0 +1,13 @@
+﻿namespace PlanningPoker.SharedKernel.Models.Decks
+{
+    /// <summary>
+    /// Represets a type of card deck.
+    /// </summary>
+    public enum DeckType
+    {
+        Fibonacci = 1,
+        FibonacciUnsure,
+        FibonacciCoffee,
+        Size
+    }
+}

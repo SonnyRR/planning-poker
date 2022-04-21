94e207aa01e66ae25b3d34206e5d2f42c68fa27d
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-04-21 19:12:29 +0300

    Added the initial code first entities & DB context.

---
+++ PlanningPoker.Persistence/Entities/BaseDeletableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanningPoker.Persistence.Entities
+{
+    public class BaseDeletableEntity<TKey> : BaseEntity<TKey>, IDeletableEntity
+         where TKey : struct, IComparable<TKey>, IEquatable<TKey>
+    {
+        public DateTimeOffset? DeletedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}

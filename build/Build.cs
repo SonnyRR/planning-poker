98e3af2750d52b515b73aad176e18135ed092374
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-05-10 19:13:29 +0300

    Addressed analyzer warnings in the NUKE build project.

--- build/Build.cs
+++ build/Build.cs
@@ -15,51 +15,58 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Compile);
+	// Support plugins are available for:
+	//   - JetBrains ReSharper        https://nuke.build/resharper
+	//   - JetBrains Rider            https://nuke.build/rider
+	//   - Microsoft VisualStudio     https://nuke.build/visualstudio
+	//   - Microsoft VSCode           https://nuke.build/vscode
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+	public static int Main() => Execute<Build>(x => x.Compile);
 
-    [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
+	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+	readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [GitVersion(Framework = "net6.0")] readonly GitVersion GitVersion;
+	[Solution] readonly Solution Solution;
+	
+#pragma warning disable IDE0051 // Remove unused private members
+#pragma warning disable RCS1213 // Remove unused member declaration.
+	[GitRepository] readonly GitRepository GitRepository;
+#pragma warning restore RCS1213 // Remove unused member declaration.
+#pragma warning restore IDE0051 // Remove unused private members
 
-    AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath TestsDirectory => RootDirectory / "tests";
-    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+	[GitVersion(Framework = "net6.0")] readonly GitVersion GitVersion;
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
-        });
+	AbsolutePath SourceDirectory => RootDirectory / "src";
+	AbsolutePath TestsDirectory => RootDirectory / "tests";
+	AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-            DotNetRestore(s => s
-                .SetProjectFile(Solution));
-        });
+	Target Clean => _ => _
+		.Before(this.Restore)
+		.Executes(() =>
+		{
+			this.SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+			this.TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+			EnsureCleanDirectory(this.ArtifactsDirectory);
+		});
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .SetAssemblyVersion(GitVersion.AssemblySemVer)
-                .SetFileVersion(GitVersion.AssemblySemFileVer)
-                .SetInformationalVersion(GitVersion.InformationalVersion)
-                .EnableNoRestore());
-        });
+	Target Restore => _ => _
+		.Executes(() =>
+		{
+			DotNetRestore(s => s
+				.SetProjectFile(this.Solution));
+		});
+
+	Target Compile => _ => _
+		.DependsOn(this.Restore)
+		.DependsOn(this.Clean)
+		.Executes(() =>
+		{
+			DotNetBuild(s => s
+				.SetProjectFile(this.Solution)
+				.SetConfiguration(this.Configuration)
+				.SetAssemblyVersion(this.GitVersion.AssemblySemVer)
+				.SetFileVersion(this.GitVersion.AssemblySemFileVer)
+				.SetInformationalVersion(this.GitVersion.InformationalVersion)
+				.EnableNoRestore());
+		});
 }
---
+++ build/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Bug", "S3903:Types should be defined in named namespaces", Justification = "Namespaces should not be required in the NUKE build project.", Scope = "module")]
+[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "Namespaces should not be required in the NUKE build project.", Scope = "module")]
+[assembly: SuppressMessage("Roslynator", "RCS1110:Declare type inside namespace.", Justification = "Namespaces should not be required in the NUKE build project.", Scope = "module")]
+[assembly: SuppressMessage("Critical Code Smell", "S2223:Non-constant static fields should not be visible", Justification = "Not required in the NUKE build project.", Scope = "module")]
+[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "Not required in the NUKE build project.", Scope = "module")]
+[assembly: SuppressMessage("Minor Code Smell", "S1104:Fields should not have public accessibility", Justification = "Not required in the NUKE build project.", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0053:Use expression body for lambda expressions", Justification = "Not required in the NUKE build project.", Scope = "module")]

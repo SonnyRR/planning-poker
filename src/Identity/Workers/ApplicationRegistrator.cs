eee2795b579b8119390f0f847467f74ef1f71778
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-05-12 11:26:19 +0300

    Registered the WebAPI routes for reverse proxy.

--- src/Identity/Workers/ApplicationRegistrator.cs
+++ src/Identity/Workers/ApplicationRegistrator.cs
@@ -23,7 +23,7 @@
 			using var scope = this.serviceProvider.CreateScope();
 
 			await RegisterApplicationsAsync(scope.ServiceProvider);
-			//await RegisterScopesAsync(scope.ServiceProvider);
+			await RegisterScopesAsync(scope.ServiceProvider);
 		}
 
 		public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
@@ -40,30 +40,62 @@
 					ConsentType = ConsentTypes.Explicit,
 					DisplayName = "Blazor code PKCE",
 					PostLogoutRedirectUris =
-						{
-							new Uri("https://localhost:5002/signout-callback-oidc")
-						},
+					{
+						new Uri("https://localhost:5002/signout-callback-oidc")
+					},
 					RedirectUris =
-						{
-							new Uri("https://localhost:5002/signin-oidc")
-						},
+					{
+						new Uri("https://localhost:5002/signin-oidc")
+					},
 					ClientSecret = "74f009d6-4600-4985-9027-c23fd047ffd5",
 					Permissions =
-						{
-							Permissions.Endpoints.Authorization,
-							Permissions.Endpoints.Logout,
-							Permissions.Endpoints.Token,
-							Permissions.GrantTypes.AuthorizationCode,
-							Permissions.ResponseTypes.Code,
-							Permissions.Scopes.Email,
-							Permissions.Scopes.Profile,
-							Permissions.Scopes.Roles
-							//Permissions.Prefixes.Scope + "api1"
-						},
+					{
+						Permissions.Endpoints.Authorization,
+						Permissions.Endpoints.Logout,
+						Permissions.Endpoints.Token,
+						Permissions.GrantTypes.AuthorizationCode,
+						Permissions.ResponseTypes.Code,
+						Permissions.Scopes.Email,
+						Permissions.Scopes.Profile,
+						Permissions.Scopes.Roles
+					},
 					Requirements =
-						{
-							Requirements.Features.ProofKeyForCodeExchange
-						}
+					{
+						Requirements.Features.ProofKeyForCodeExchange
+					}
+				});
+			}
+
+			if (await manager.FindByClientIdAsync("c257f553-8ce4-4225-8447-c9b7aa81f465") == null)
+			{
+				var descriptor = new OpenIddictApplicationDescriptor
+				{
+					ClientId = "c257f553-8ce4-4225-8447-c9b7aa81f465",
+					ClientSecret = "e60339d0-2a1b-4d22-8878-b0c8ba95f30e",
+					Permissions =
+					{
+						Permissions.Endpoints.Introspection
+					}
+				};
+
+				await manager.CreateAsync(descriptor);
+			}
+		}
+
+		static async Task RegisterScopesAsync(IServiceProvider provider)
+		{
+			var manager = provider.GetRequiredService<IOpenIddictScopeManager>();
+
+			if (await manager.FindByNameAsync("planning-poker-api") is null)
+			{
+				await manager.CreateAsync(new OpenIddictScopeDescriptor
+				{
+					DisplayName = "Planning Poker API access.",
+					Name = "planning-poker-api",
+					Resources =
+					{
+						"c257f553-8ce4-4225-8447-c9b7aa81f465"
+					}
 				});
 			}
 		}

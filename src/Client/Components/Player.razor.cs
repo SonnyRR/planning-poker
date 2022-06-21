d17aacd663f7f3020d52f25c59448b614ae730c8
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-06-21 19:01:09 +0300

    Added animation when revealing a player's vote.

---
+++ src/Client/Components/Player.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace PlanningPoker.Client.Components
+{
+	using Microsoft.AspNetCore.Components;
+	using System;
+
+	public partial class Player
+	{
+		public bool HasVoted { get; set; }
+
+		[Parameter]
+		public string PlayerName { get; set; }
+
+		[Parameter]
+		public Guid PlayerId { get; set; }
+
+		public void ChangeMe()
+		{
+
+		}
+	}
+}
---
+++ src/Client/EventHandlers.cs
@@ -0,0 +1,11 @@
+﻿namespace PlanningPoker.Client
+{
+	using Microsoft.AspNetCore.Components;
+	using System;
+
+	[EventHandler("ontransitionend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+	[EventHandler("onanimationend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+	public static class EventHandlers
+	{
+	}
+}

65c6772b59bda210972677b8e7478256c37e5287
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2023-11-14 18:37:18 +0200

    Introduce click handler for 'Card' component

--- src/Client/Components/Card.razor.cs
+++ src/Client/Components/Card.razor.cs
@@ -1,13 +1,47 @@
 ﻿namespace PlanningPoker.Client.Components
 {
-	using Microsoft.AspNetCore.Components;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Web;
 
-	public partial class Card
+    public partial class Card
 	{
+        private bool clicking;
+
 		[Parameter]
 		public string UnicodeValue { get; set; }
 
 		[Parameter]
 		public int Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the click callback.
+        /// </summary>
+        /// <value>The click callback.</value>
+        [Parameter]
+        public EventCallback<MouseEventArgs> Click { get; set; }
+
+        /// <summary>
+        /// Handles the <see cref="E:Click" /> event.
+        /// </summary>
+        /// <param name="args">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+        public virtual async Task OnClick(MouseEventArgs args)
+        {
+            if (this.clicking)
+            {
+                return;
+            }
+
+            try
+            {
+                this.clicking = true;
+
+                await this.Click.InvokeAsync(args);
+            }
+            finally
+            {
+                this.clicking = false;
+            }
+        }
 	}
 }
